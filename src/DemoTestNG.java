df13d32e59ba23fb6a04cef2c47d5c895a355697
Rubel <bittechcorp@gmail.com>
2013-11-02 22:00:33 -0400

    add new project

---
+++ src/DemoTestNG.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class DemoTestNG 
+{
+	FirefoxDriver driver = new FirefoxDriver();
+	
+	@BeforeTest
+	public void start()
+	{
+		driver.get("http://www.facebook.com");
+	}
+	
+	@AfterTest
+	public void end()
+	{
+		driver.quit();
+	}
+	
+	@Test
+	public void login()
+	{
+		driver.findElement(By.id("email")).sendKeys("asad@bittechusa.com");
+	}
+
+}
